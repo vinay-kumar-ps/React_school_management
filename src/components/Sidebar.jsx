455cd7ef606cc6114796342ce13390e442e06217
vinay-kumar-ps <vinaykumarps321@gmail.com>
2025-03-11 21:03:05 +0530

    learning

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,33 @@
 import { Nav } from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faHouse ,faGraduationCap,faUserTie,faBook, faImagePortrait} from '@fortawesome/free-solid-svg-icons';
+
 
 
 const Sidebar = () => {
 return(
     <div className="sidebar">
   <Nav  defaultActiveKey= "/" className="flex-column">
-            <Nav.Link href="/">Dashboard</Nav.Link>
-            <Nav.Link href="/stduents">stduents</Nav.Link>
-            <Nav.Link href="/staffs">staffs</Nav.Link>
-            <Nav.Link href="/library">library</Nav.Link>
-            <Nav.Link href="/accounts">accounts</Nav.Link>
+            <Nav.Link href="/">
+            <FontAwesomeIcon icon={faHouse} />
+            <span className= "d-none d-md-inline">Dashboard</span>
+            </Nav.Link>
+            <Nav.Link href="/">
+            <FontAwesomeIcon icon={faGraduationCap} />
+            <span className= "d-none d-md-inline">students</span>
+            </Nav.Link>
+            <Nav.Link href="/">
+            <FontAwesomeIcon icon={faUserTie} />
+            <span className= "d-none d-md-inline">staff</span>
+            </Nav.Link>        
+            <Nav.Link href="/">
+            <FontAwesomeIcon icon={faBook} />
+            <span className= "d-none d-md-inline">library</span>
+            </Nav.Link>           
+            <Nav.Link href="/">
+            <FontAwesomeIcon icon={faImagePortrait} />
+            <span className= "d-none d-md-inline">accounts</span>
+            </Nav.Link>
                        
           </Nav>
         </div>
