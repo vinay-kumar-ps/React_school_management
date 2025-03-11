c2ad5b524fd9f5ef004de6975a7170be1cb12d09
vinay-kumar-ps <vinaykumarps321@gmail.com>
2025-03-11 11:46:05 +0530

    learning

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello School App</h1>
+      <Button variant="primary">Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
